2e9124a01b8411234c5e5778928540704384a879
Ryan Flaherty <Ryan.Flaherty2@gmail.com>
2014-03-28 01:32:33 -0500

    still evolving

--- AutoDeployServer.rb
+++ AutoDeployServer.rb
@@ -3,6 +3,13 @@ require 'json'
 require_relative 'LoadConfig'
 
 config = load()
+#this will start up the servers before we start listening.
+config["repos"].each do |repo|
+  puts "Executing startScript for "+repo["name"]
+  system "cd "+repo["workingDir"]+" && "+repo["startScript"]+" &"
+  puts "Done"
+end
+
 
 #TODO: load port from config
 set :port, 8765  
@@ -10,13 +17,17 @@ set :port, 8765
 post '/payload'  do
   #if its a push, we care about it.
   if env['HTTP_X_GITHUB_EVENT']=='push' then
-        #Printing this stuff so i can debug easily
-        webHook = JSON.parse(params[:payload])
-        #lets check out what repo it was
-        repo = webHook['repository']['url']
-        script = config[repo]['script']
-	puts script
-	puts "Detected push to "+repo+" running script at "+script
-        system script
+    #Printing this stuff so i can debug easily
+    webHook = JSON.parse(params[:payload])
+    #lets check out what repo it was
+    payloadRepo = webHook['repository']['url']
+    config["repos"].each do |repo|
+      if (repo["name"]==payloadRepo) then
+        script = repo['script']
+        puts script
+        puts "Detected push to "+payloadRepo+" running script at "+script
+        system "cd "+repo["workingDir"]+" && "+repo["script"]+" &"
+      end
+    end
   end
 end
